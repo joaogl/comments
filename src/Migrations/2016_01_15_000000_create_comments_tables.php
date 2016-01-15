4661c44fa1f7eec567203ff17be7c15bc821d5a0
Joaogl <joaoguerralourenco@hotmail.com>
2016-01-15 00:18:47 +0000

    Added the first version of the migrations.

---
+++ src/Migrations/2016_01_15_000000_create_comments_tables.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use \jlourenco\base\Database\Blueprint;
+
+class CreateCommentsTables extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('Comment', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent')->unsigned()->nullable();
+            $table->text('comment');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->creation();
+
+            $table->foreign('parent')->references('id')->on('Comment');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::drop('Comment');
+
+    }
+
+}
