66fabe1ad4a8977a0933f8310ee6dcb38a72cbc3
vagrant <joaoguerralourenco@hotmail.com>
2016-06-29 16:59:36 +0000

    Update

--- src/views/public/comments/partials/list.blade.php
+++ src/views/public/comments/partials/list.blade.php
@@ -4,7 +4,7 @@
     <div class="media">
         <a class="pull-left" href="#">
             @if($comment->created_by != null && $comment->created_by->pic)
-                <img src="{!! url('/').'/uploads/users/'.$comment->created_by->pic !!}" alt="img" class="img-max img-rounded"/>
+                <img src="{!! url('/').'/uploads/users/'.$comment->created_by->pic !!}" alt="img" class="img-max img-rounded" width="64"/>
             @else
                 <img src="http://placehold.it/64x64" alt="..." class="img-max img-rounded" />
             @endif
@@ -13,7 +13,7 @@
             <h4 class="media-heading">
                 {{ $comment->created_by != null ? $comment->created_by->first_name . ' ' . $comment->created_by->last_name : ''  }}
                 <small>{{ $comment->created_at->format('M d, Y \a\t h:i a') }}</small>
-                @if (Sentinel::inRole('admin'))
+                @if (Sentinel::check() && Sentinel::inRole('admin'))
                     <small><a href="{{ route('delete-comment', $comment->id) }}">delete</a></small>
                 @endif
             </h4>
